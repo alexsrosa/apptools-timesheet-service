2620637b9f975b6a5c104be08f85d049affa3ca0
Alex S. Rosa <alexsros@gmail.com>
2017-07-18 21:14:56 -0300

    new feature

--- src/main/java/br/com/tools/timesheet/service/sat/ViewTimeSheetService.java
+++ src/main/java/br/com/tools/timesheet/service/sat/ViewTimeSheetService.java
@@ -14,14 +14,23 @@ public class ViewTimeSheetService {
 	@Autowired
 	private ViewTimeSheetRepository repository;
 
-	public List<ViewTimeSheet> findByTarefa(String tarefa) {
-		return this.repository.findByTarefa(tarefa);
-	}
-	
+
 	public List<ViewTimeSheet> findByMatricula(String matricula) {
 		return this.repository.findByMatricula(matricula);
 	}
 
+	public List<ViewTimeSheet> findByMatriculaWithPeriodo(String matricula, String data1, String data2) {
+		return this.repository.findByMatriculaWithPeriodo(matricula, data1, data2);
+	}
+	
+	public List<ViewTimeSheet> findByTarefa(String tarefa) {
+		return this.repository.findByTarefa(tarefa);
+	}
+
+	public List<ViewTimeSheet> findByTarefaWithPeriodo(String tarefa, String data1, String data2) {
+		return this.repository.findByTarefaWithPeriodo(tarefa, data1, data2);
+	}
+	
 	public List<ViewTimeSheet> findByData(String data1, String data2) {
 		return this.repository.findByData(data1, data2);
 	}
