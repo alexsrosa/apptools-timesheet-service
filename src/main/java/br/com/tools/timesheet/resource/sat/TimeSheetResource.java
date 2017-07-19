85b94a43f1c41159ec571c6e6778759dc1994b7f
Alex S. Rosa <alexsros@gmail.com>
2017-07-18 21:14:36 -0300

    new feature

--- src/main/java/br/com/tools/timesheet/resource/sat/TimeSheetResource.java
+++ src/main/java/br/com/tools/timesheet/resource/sat/TimeSheetResource.java
@@ -3,11 +3,9 @@ package br.com.tools.timesheet.resource.sat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tools.timesheet.domain.sat.ViewTimeSheet;
@@ -25,9 +23,21 @@ public class TimeSheetResource {
 		return this.service.findByMatricula(matricula);
 	}
 
+	@RequestMapping(value = "/matricula/{matricula}/periodo/{data1}/{data2}", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+	public List<ViewTimeSheet> findByMatriculaWithPeriodo(@PathVariable String matricula, @PathVariable String data1,
+			@PathVariable String data2) {
+		return this.service.findByMatriculaWithPeriodo(matricula, data1, data2);
+	}
+
 	@RequestMapping(value = "/tarefa/{tarefa}", method = RequestMethod.GET)
 	public List<ViewTimeSheet> findByTarefa(@PathVariable String tarefa) {
 		return this.service.findByTarefa(tarefa);
+	}
+	
+	@RequestMapping(value = "/tarefa/{matricula}/periodo/{data1}/{data2}", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+	public List<ViewTimeSheet> findByTarefaWithPeriodo(@PathVariable String tarefa, @PathVariable String data1,
+			@PathVariable String data2) {
+		return this.service.findByTarefaWithPeriodo(tarefa, data1, data2);
 	}
 
 	@RequestMapping(value = "/periodo/{data1}/{data2}", method = RequestMethod.GET)
@@ -35,10 +45,13 @@ public class TimeSheetResource {
 		return this.service.findByData(data1, data2);
 	}
 
-	@RequestMapping(value = "/busca", method = RequestMethod.GET)
-	public List<ViewTimeSheet> busca(@ModelAttribute("viewTimeSheet") ViewTimeSheet timeSheet,
-			@RequestParam(value = "data2") String data2) {
-		return this.service.findByMatricula(timeSheet.getMatricula());
-	}
+	/*
+	 * @RequestMapping(value = "/busca", method = RequestMethod.GET) public
+	 * List<ViewTimeSheet> busca(@ModelAttribute("viewTimeSheet") ViewTimeSheet
+	 * timeSheet,
+	 * 
+	 * @RequestParam(value = "data2", required = false) String data2) { return
+	 * this.service.busca(timeSheet, data2); }
+	 */
 
 }
