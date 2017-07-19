bf7e1fdde4c422a615beb133a559d1705ccb8590
Alex S. Rosa <alexsros@gmail.com>
2017-07-18 21:14:08 -0300

    new feature

--- src/main/java/br/com/tools/timesheet/repositories/sat/ViewTimeSheetRepository.java
+++ src/main/java/br/com/tools/timesheet/repositories/sat/ViewTimeSheetRepository.java
@@ -4,25 +4,35 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.tools.timesheet.domain.sat.ViewTimeSheet;
 
 public interface ViewTimeSheetRepository extends Repository<ViewTimeSheet, Integer> {
-	
+
 	Iterable<ViewTimeSheet> findAll();
 
 	long count();
-	
+
 	boolean existsById(Integer arg0);
-	
+
 	List<ViewTimeSheet> findByMatricula(String matricula);
 	
+	@Query(value = "SELECT * FROM SAT_VIEW_POA where matricula =:matricula and dt_timesheet between CONVERT(VARCHAR(10), :data1, 103) and CONVERT(VARCHAR(10), :data2, 103)", nativeQuery = true)
+	List<ViewTimeSheet> findByMatriculaWithPeriodo(@Param("matricula") String matricula, @Param("data1") String data1,
+			@Param("data2") String data2);
+
 	List<ViewTimeSheet> findByTarefa(String tarefa);
 	
-	@Query(value = "SELECT * FROM SAT_VIEW_POA where dt_timesheet between CONVERT(VARCHAR(10), ?1, 103) and CONVERT(VARCHAR(10), ?2, 103)", nativeQuery = true)
-	List<ViewTimeSheet> findByData(String date1, String data2);
-	
-	@Query(value = "SELECT * FROM SAT_VIEW_POA where dt_timesheet between CONVERT(VARCHAR(10), ?1, 103) and CONVERT(VARCHAR(10), ?2, 103) and tarefa =?3", nativeQuery = true)
-	List<ViewTimeSheet> findByDataWithTarefa(String date1, String data2, String tarefa);
-		
+	@Query(value = "SELECT * FROM SAT_VIEW_POA where tarefa =:tarefa and dt_timesheet between CONVERT(VARCHAR(10), :data1, 103) and CONVERT(VARCHAR(10), :data2, 103)", nativeQuery = true)
+	List<ViewTimeSheet> findByTarefaWithPeriodo	(@Param("tarefa") String tarefa, @Param("data1") String data1,
+			@Param("data2") String data2);
+
+	@Query(value = "SELECT * FROM SAT_VIEW_POA where dt_timesheet between CONVERT(VARCHAR(10), :data1, 103) and CONVERT(VARCHAR(10), :data2, 103)", nativeQuery = true)
+	List<ViewTimeSheet> findByData(@Param("data1") String data1,@Param("data2") String data2);
+
+
+
+
+
 }
